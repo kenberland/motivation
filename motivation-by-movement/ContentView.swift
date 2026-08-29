1d6445f3219d54d88458341b86ce6136eab16d5c
Ken Berland <berland.ken@gene.com>
2026-08-29 09:43:07 -0700

    Initial Commit

---
+++ motivation-by-movement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  motivation-by-movement
+//
+//  Created by berlank1 on 8/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ motivation-by-movement/motivation_by_movementApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivation_by_movementApp.swift
+//  motivation-by-movement
+//
+//  Created by berlank1 on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct motivation_by_movementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
